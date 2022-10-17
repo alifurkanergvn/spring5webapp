7d36e872df2d34a7cb237e0c2fd957d6251de601
Ali Furkan ERGUVEN <alifurkanerguven@gmail.com>
2022-10-17 23:30:55 +0300

    Worked basic Spring MCV Controller we did   @RequestMapping and  model.addAttribute on 21.videos course ending source is adding-controller-v2

---
+++ src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){  //Spring MVC yapıya sahip controllerimiz var model şeklinde çağırarak
+        // Modelimizi getiriyoruz. Burası view e göndereceğimizdir. View de modeli kopyalayacaktır.
+
+        model.addAttribute("books",bookRepository.findAll()) ; //attribute ismini,attribute değerini yazdık.
+        // Burası bize databaseden Book listesi verip books attribute na ekledi
+        // Bu aşamaya kadar Spring MCV Controller ın temel seviyesini gördük
+
+        return "books";
+    }
+}
